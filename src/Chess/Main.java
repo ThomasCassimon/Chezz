48fec8a70bc35a9f18fc755700565516fa692dd2
Astrid <astrid.vanneste1@gmail.com>
2017-02-22 19:45:06 +0100

    test

--- src/Chess/Main.java
+++ src/Chess/Main.java
@@ -20,7 +20,7 @@ public class Main
 		MainWindow mw = new MainWindow();
 
 		System.out.println("Ola Ola Ola!");
-		System.out.println("Bonjour!");
+		System.out.println("Bonjour");
 		System.out.println("Bonjour");
 	}
 }
