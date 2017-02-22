23071db74a9a28963b5adf82e2143c5f09337ed6
Astrid <astrid.vanneste1@gmail.com>
2017-02-22 20:47:51 +0100

    UI forming chess board

--- src/Chess/UI/MainWindow.java
+++ src/Chess/UI/MainWindow.java
@@ -2,6 +2,7 @@ package Chess.UI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Astrid on 22/02/2017.
@@ -11,24 +12,59 @@ public class MainWindow extends JFrame
 	private JPanel panel = new JPanel();
 	private Tile tiles[] = new Tile[64];
 
+	/**
+	 *	Images
+	 */
+	private ImageIcon white_Pawn = new ImageIcon("PW.png");
+
+	/**
+	 *	Colors
+	 */
+	private Color colorBlack = Color.BLACK;
+	private Color colorWhite = Color.WHITE;
+
+
+
 	public MainWindow()
 	{
 		 super("Chezz!");
 
-		 int i;
+		 int row,col, arrayIndex;
 
-		 setSize(400,400);
+		 setSize(800,800);
 		 setResizable(false);
 		 setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		 panel.setLayout(new GridLayout(8,8));
 
 
-		 for (i=0;i<64;i++)
+		 for (row=0; row <8; row++)
 		 {
-		 	tiles[i] = new Tile();
-		 	panel.add(tiles[i]);
+
+		 	for (col=0;col<8;col++)
+			{
+				arrayIndex = (row * 8) + col;
+
+
+				tiles[arrayIndex] = new Tile();
+
+				if ( (row + col)%2 == 0)
+				{
+					tiles[arrayIndex].setBackground(colorBlack);
+				}
+				else
+				{
+					tiles[arrayIndex].setBackground(colorWhite);
+				}
+
+				tiles[arrayIndex].setOpaque(true);
+				tiles[arrayIndex].setBorderPainted(false);
+
+				panel.add(tiles[arrayIndex]);
+			}
+
 		 }
 
+		 add(panel);
 		 setVisible(true);
 	}
 
